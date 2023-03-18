fe8af357e16eb870b567d0f02ee67025827ef72c
ethan <gohho1008@gmail.com>
2023-03-18 13:06:36 +0900

    create_calculator

---
+++ native_application/lib/calculator_screen.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String userInput = '';
+  String result = '0';
+
+  List<String> buttonList = [
+    'AC',
+    '(',
+    ')',
+    '/',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    'C',
+    '0',
+    '.',
+    '=',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    userInput,
+                    style: const TextStyle(fontSize: 32, color: Colors.white),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    result,
+                    style: const TextStyle(
+                      fontSize: 48,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const Divider(color: Colors.white),
+          Expanded(
+              child: Container(
+            padding: const EdgeInsets.all(10),
+            child: GridView.builder(
+              itemCount: buttonList.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 12,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return createCutomButton(buttonList[index]);
+              },
+            ),
+          )),
+        ],
+      ),
+    );
+  }
+
+  Widget createCutomButton(String btnText) {
+    return InkWell(
+      splashColor: const Color(0xFF1d2630),
+      onTap: () {
+        setState(() {
+          handleButtons(btnText);
+        });
+      },
+      child: Ink(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white.withOpacity(0.1),
+                blurRadius: 4,
+                spreadRadius: 0.5,
+                offset: const Offset(-3, -3),
+              ),
+            ]),
+        child: Center(
+          child: Text(
+            btnText,
+            style: const TextStyle(
+                color: Colors.blue, fontSize: 32, fontWeight: FontWeight.w500),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void handleButtons(String btnText) {
+    if (btnText == 'AC') {
+      userInput = '';
+      result = '0';
+      return;
+    }
+
+    if (btnText == 'C') {
+      if (userInput.isNotEmpty) {
+        userInput = userInput.substring(0, userInput.length - 1);
+        return;
+      } else {
+        return;
+      }
+    }
+
+    if (btnText == '=') {
+      result = calculator();
+      userInput = result;
+      if (userInput.endsWith('.0')) {
+        userInput = userInput.replaceAll('.0', '');
+        
+        if (result.endsWith('.0')) {
+          result = result.replaceAll('.0', '');
+        }
+        return;
+      }
+    }
+
+    userInput = userInput + btnText;
+  }
+
+  String calculator() {
+    try {
+      var exp = Parser().parse(userInput);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+
+      return evaluation.toString();
+    } catch (e) {
+      return 'Error';
+    }
+  }
+}
